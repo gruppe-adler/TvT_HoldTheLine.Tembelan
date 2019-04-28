a12d05f4195ae3fe9884ea4934d7621aaa92bc2b
McDiod <hobbeldobbel@gmail.com>
2019-04-28 15:23:14 +0200

    add mission info

--- USER/missionInfo.hpp
+++ USER/missionInfo.hpp
@@ -2,7 +2,7 @@
 *   Ist in description.ext eingebunden, kann also auch für andere Configs benutzt werden.
 */
 
-author = "Name für Gruppe Adler";                                               //Missionsersteller (Du)
-onLoadName = "TVT TEMPLATE";                                                    //Name der Mission
+author = "XiviD für Gruppe Adler";                                               //Missionsersteller (Du)
+onLoadName = "HOLD THE LINE";                                                    //Name der Mission
 onLoadMission = "";                                                             //Beschreibung der Mission (wird im Ladebildschirm unterhalb des Ladebildes angezeigt)
 loadScreen = "data\loadpic.paa";                                                //Ladebild
